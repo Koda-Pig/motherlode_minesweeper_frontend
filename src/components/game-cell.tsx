73125f5c8f26ad278c903611ba163d5d2af2d3e1
Josh Koter <josh.koter@gmail.com>
2026-01-13 00:07:23 +0200

    ...

--- src/components/game-cell.tsx
+++ src/components/game-cell.tsx
@@ -1,23 +1,37 @@
-import { forwardRef } from "react";
+import { forwardRef, useState } from "react";
 import { cn } from "@/lib/utils";
 
 interface GameCellProps {
   className?: string;
 }
 
-const GameCell = forwardRef<HTMLDivElement, GameCellProps>(
+const GameCell = forwardRef<HTMLButtonElement, GameCellProps>(
   ({ className, ...props }, ref) => {
+    const isMine = Math.random() < 0.1;
+    const [isClicked, setIsClicked] = useState(false);
+
     return (
-      <div
+      <button
         ref={ref}
         className={cn(
           className,
-          "w-20 h-20 border-2 border-foreground flex rounded-lg p-2"
+          "w-20 h-20 border-2 border-foreground flex rounded-lg p-2 transition-all",
+          isClicked
+            ? "pointer-events-none"
+            : "hover:border-white hover:scale-105"
         )}
         {...props}
+        onClick={() => setIsClicked(true)}
       >
-        <img src="plumbob.webp" alt="plumbob" className="mx-auto" />
-      </div>
+        <img
+          src={isMine ? "mine.png" : "plumbob.webp"}
+          alt="...mine?"
+          className={cn(
+            "mx-auto transition-opacity",
+            isClicked ? "opacity-100" : "opacity-0"
+          )}
+        />
+      </button>
     );
   }
 );
